85dbb28c785c013009c8c699a71b691b558cd390
Daniel Fourie <bobfourie@hotmail.com>
2016-01-21 15:57:02 +0200

    bat changed

--- addtogit.bat
+++ addtogit.bat
@@ -1,14 +1,14 @@
-ECHO add the remote repos as a shortcut to snip
+:: add the remote repos as a shortcut to snip
 git remote add snip https://github.com/bobfourie/subsassnip
 
-ECHO Adds the files in the local repository and stages them for commit
+:: Adds the files in the local repository and stages them for commit
 git add .
 
-ECHO Commits the tracked changes and prepares them to be pushed to a remote repository
+:: Commits the tracked changes and prepares them to be pushed to a remote repository
 @echo off
 set /p commsg="Enter commit msg: "
 git commit -m "%commsg%"
 
 @echo on
-ECHO Pushes the changes in your local repository up to the remote repository you specified as the origin
+:: Pushes the changes in your local repository up to the remote repository you specified as the origin
 git push origin master
